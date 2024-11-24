34071b2e718aece6ddf9a1521113cc7b7e3adb47
Chirag Soni <csoni0693@gmail.com>
2024-11-24 13:34:45 +0530

    library

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target:"http://localhost:5000",
+        target:"https://library-management-0b5r.onrender.com/",
       }
     }
   }
